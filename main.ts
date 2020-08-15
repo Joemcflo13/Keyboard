e4b124183d78192984717a7a1fd05a7c356cb441
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-08-14 19:19:37 -0600

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,39 @@
 
-//%icon="\f07b" color=#0344ab weight=2
+//%icon="\uf11c" color=#0344ab weight=95
+//% groups='["keyboards"]'
 namespace Keyboard {
     /**
-     * @param prompt this is my parameter, eg: "What do you want to ask?"
-     * keyboard so you can write on screen with a prompt
+     * @param prompt this is my parameter, eg: "WHAT DO YOU WANT TO ASK?"
+     * keyboard so you can write on screen with a prompt, when the letter pops up that you need to use, press a.
      */
     //% blockId=textkeyboard block="Keyboard with prompt: $prompt"
-    export function textkey(prompt: string) : void {
+    //% group="keyboards"
+    export function textkey(prompt: string): void {
         basic.showString(prompt)
+        basic.showString("A")
+        if (input.buttonIsPressed(Button.A)) {
+            let Letter: string "A"
+            return Letter;
+        } else {
+            let Letter: string "B"
+            return Letter
+        }
+        
     }
     /**
      * keyboard so you can write on screen without a prompt
      */
     //% blockId=justkeyboard block="Keyboard"
-    export function justkey() : void {
+    //% group="keyboards"
+    export function justkey(): void {
         
+    }
+    /**
+     * keyboard with slang, or phrase, to help you, so if you have radio on you could send a text with a phrase.
+     */
+    //% blockId=slang block="slang keyboard"
+    //% group="keyboards"
+    export function slang(slang: string): void {
+        basic.showString("Sup")
     }
 }
