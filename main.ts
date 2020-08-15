1d0880f9b1193d568d8549757864c8e8fc3ecca4
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-08-14 18:01:15 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 
 //%icon="\f044" color=#0344ab weight=2
-namespace Save_text {
-    
-
-    export function textsave(save: string) : string {
-        const string = save
+namespace Keyboard {
+    /**
+     * keyboard so you can write on screen
+     */
+    //% blockId=textkey block="Keyboard"
+    export function textkey(save: string) : string {
+        
     }
 }
