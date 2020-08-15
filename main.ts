83c4b49e95eeafaaf617fef939772df0856efa2f
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-08-14 18:21:16 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
 //%icon="\f07b" color=#0344ab weight=2
 namespace Keyboard {
     /**
+     * @param prompt this is my parameter, eg: "What do you want to ask?"
      * keyboard so you can write on screen with a prompt
      */
     //% blockId=textkeyboard block="Keyboard with prompt: $prompt"
