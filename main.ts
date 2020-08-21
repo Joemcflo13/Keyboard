07f37cc5f3777d14e8adf9b725512048f52ae8ce
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-08-21 16:44:59 -0600

    bug fixes and minor changes

--- main.ts
+++ main.ts
@@ -482,11 +482,16 @@ namespace Keyboard {
         basic.showString("Y")
         while (true) {
             if (input.buttonIsPressed(Button.A)) {
+                basic.clearScreen()
                 return true;
             } else if (input.buttonIsPressed(Button.B)) {
                 basic.showString("N")
                 while (true) {
                     if (input.buttonIsPressed(Button.A)) {
+                        basic.clearScreen()
+                        return false;
+                    } else if (input.buttonIsPressed(Button.B)) {
+                        basic.clearScreen()
                         return false;
                     }
                 }
@@ -503,11 +508,16 @@ namespace Keyboard {
         basic.showString("Y")
         while (true) {
             if (input.buttonIsPressed(Button.A)) {
+                basic.clearScreen()
                 return true;
             } else if (input.buttonIsPressed(Button.B)) {
                 basic.showString("N")
                 while (true) {
                     if (input.buttonIsPressed(Button.A)) {
+                        basic.clearScreen()
+                        return false;
+                    } else if (input.buttonIsPressed(Button.B)) {
+                        basic.clearScreen()
                         return false;
                     }
                 }
